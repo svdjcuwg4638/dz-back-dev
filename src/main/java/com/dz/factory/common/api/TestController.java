8cc3439b152c0739acddf59f3de90a4c8fef33c5
svdjcuwg4638 <kdhwon1@gmail.com>
2023-10-04 00:14:27 +0900

    테스트추가

--- src/main/java/com/dz/factory/common/api/TestController.java
+++ src/main/java/com/dz/factory/common/api/TestController.java
@@ -1,14 +1,32 @@
 package com.dz.factory.common.api;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class TestController {
 
-	@RequestMapping("/home")
-	public String home() {
-		return "index";
-	}
+    @Value("${JAWSDB_MARIA_URL}")
+    private String jawsDbMariaUrl;
+
+    @Value("${JawsDB_PASSWORD}")
+    private String jawsDbPassword;
+
+    @Value("${JawsDB_URL}")
+    private String jawsDbUrl;
+
+    @Value("${JawsDB_USER}")
+    private String jawsDbUser;
+
+    @RequestMapping("/home")
+    public String home(Model model) {
+        model.addAttribute("jawsDbMariaUrl", jawsDbMariaUrl);
+        model.addAttribute("jawsDbPassword", jawsDbPassword);
+        model.addAttribute("jawsDbUrl", jawsDbUrl);
+        model.addAttribute("jawsDbUser", jawsDbUser);
+        return "index";
+    }
 	
 }
