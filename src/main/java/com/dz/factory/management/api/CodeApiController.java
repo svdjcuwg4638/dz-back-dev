edbd9932d1e6ba4c08baeac4831ce033307c6876
svdjcuwg4638 <kdhwon1@gmail.com>
2023-10-03 21:29:37 +0900

    ㅅㅅ

--- src/main/java/com/dz/factory/management/api/CodeApiController.java
+++ src/main/java/com/dz/factory/management/api/CodeApiController.java
@@ -19,6 +19,7 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 @RestController
 public class CodeApiController {
+
 	private final CodeService codeService;
 
 	@GetMapping("/code/all")
