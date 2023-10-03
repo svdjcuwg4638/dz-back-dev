651fa11248c0bbffa9e0391244b4cda271c5e669
svdjcuwg4638 <kdhwon1@gmail.com>
2023-10-04 00:19:43 +0900

    ㅅ

--- src/main/java/com/dz/factory/common/api/TestController.java
+++ src/main/java/com/dz/factory/common/api/TestController.java
@@ -1,11 +1,10 @@
 package com.dz.factory.common.api;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class TestController {
 
     @Value("${JAWSDB_MARIA_URL}")
@@ -20,13 +19,38 @@ public class TestController {
     @Value("${JawsDB_USER}")
     private String jawsDbUser;
 
-    @RequestMapping("/home")
-    public String home(Model model) {
-        model.addAttribute("jawsDbMariaUrl", jawsDbMariaUrl);
-        model.addAttribute("jawsDbPassword", jawsDbPassword);
-        model.addAttribute("jawsDbUrl", jawsDbUrl);
-        model.addAttribute("jawsDbUser", jawsDbUser);
-        return "index";
+    @GetMapping("/config")
+    public ConfigResponse getConfig() {
+        return new ConfigResponse(jawsDbMariaUrl, jawsDbPassword, jawsDbUrl, jawsDbUser);
     }
-	
+
+    public static class ConfigResponse {
+        private final String jawsDbMariaUrl;
+        private final String jawsDbPassword;
+        private final String jawsDbUrl;
+        private final String jawsDbUser;
+
+        public ConfigResponse(String jawsDbMariaUrl, String jawsDbPassword, String jawsDbUrl, String jawsDbUser) {
+            this.jawsDbMariaUrl = jawsDbMariaUrl;
+            this.jawsDbPassword = jawsDbPassword;
+            this.jawsDbUrl = jawsDbUrl;
+            this.jawsDbUser = jawsDbUser;
+        }
+
+        public String getJawsDbMariaUrl() {
+            return jawsDbMariaUrl;
+        }
+
+        public String getJawsDbPassword() {
+            return jawsDbPassword;
+        }
+
+        public String getJawsDbUrl() {
+            return jawsDbUrl;
+        }
+
+        public String getJawsDbUser() {
+            return jawsDbUser;
+        }
+    }
 }
